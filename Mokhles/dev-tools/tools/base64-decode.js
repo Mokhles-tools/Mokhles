5d28a226ed1b99d8549a939d2ff058a48a78f51b
CHARGE <chargexix@gmail.com>
2025-12-23 20:36:22 +0600

    Delete Mokhles directory

--- Mokhles/dev-tools/tools/base64-decode.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('decodeBtn').addEventListener('click', () => {
-    const input = document.getElementById('base64Input').value;
-    try {
-        document.getElementById('plainOutput').value = decodeURIComponent(escape(atob(input)));
-    } catch (e) {
-        alert("Invalid Base64 string.");
-    }
-});
